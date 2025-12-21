4a09088ebc714c842a640a9f28df521449fcafc1
Anders Birkenes <datagutten@datagutten.net>
2025-12-21 14:41:09 +0100

    Add basic AceAddon setup

---
+++ GFW_HuntersHelper/addon/common.lua
@@ -0,0 +1,8 @@
+local addonName = ...
+---@class HuntersHelper
+local addon = _G.LibStub("AceAddon-3.0"):NewAddon(addonName)
+
+addon.name = 'Hunters Helper'
+addon.real_name = addonName
+addon.internal_name = 'GFW_HuntersHelper'
+addon.version = '@version@'
